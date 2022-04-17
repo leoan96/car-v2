12f8e1561cc286ebfc3fcc19e0d790d5db321a2f
mun kit <mk@muns-MacBook-Pro.local>
2022-04-17 22:23:41 +0800

    set up circleci

---
+++ .platform/confighooks/prebuild/01login.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+USER=$(/opt/elasticbeanstalk/bin/get-config environment -k USER)
+PASSWD=$(/opt/elasticbeanstalk/bin/get-config environment -k PASSWD)
+docker login -u $USER -p $PASSWD          
